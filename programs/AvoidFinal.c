1467153d64313461c6293c34e63feb653f7d2bea
Lhaldenby <32489947+Lhaldenby@users.noreply.github.com>
2019-11-08 23:10:47 +0000

    Create AvoidFinal.c to be checked by gabe before tuesday

---
+++ programs/AvoidFinal.c
@@ -0,0 +1,33 @@
+bool avoidBool = false;
+bool backUp = false;
+
+task avoid(){
+while (true){
+
+  //detect object and start to reverse
+  if (getUSDistance(leftSonar) < 10 || getUSDistance(rightSonar) < 10){
+    bool avoidBool = true;
+    bool backUp = true;
+  }
+
+  //reverse
+  if(backUp && (getUSDistance(leftSonar) < 20 || getUSDistance(rightSonar) < 20)){
+    lSpeed = -botSpeed;
+    rSpeed = -botSpeed;
+  }
+  
+  //turn right 
+  if(avoidBool && (getUSDistance(leftSonar) < 20 || getUSDistance(rightSonar) < 20)){
+    lSpeed = botSpeed;
+    rSpeed = -botSpeed;
+  }
+  
+  //spiral around
+  if(avoidBool && (getUSDistance(leftSonar) > 20 || getUSDistance(rightSonar) > 20)){
+    backUP = false;
+    lSpeed = 8;
+    rSpeed = 12;
+  }
+    
+}
+}
